8345241309145c77571e160f4d1e83e38b9c38a2
AteebXYZ <130910481+AteebXYZ@users.noreply.github.com>
2023-12-23 19:49:13 +0300

    Update reset.bat

--- reset.bat
+++ reset.bat
@@ -1,14 +1,14 @@
 @echo off
 
 :: BatchGotAdmin
-REM
+
     IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
 >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
 ) ELSE (
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
-REM --> If error flag set, we do not have admin.
+
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     goto UACPrompt
