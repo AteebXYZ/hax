0139073c4b71f3b0ccaf27a646d86dc99c088316
AteebXYZ <130910481+AteebXYZ@users.noreply.github.com>
2023-12-27 17:45:27 +0300

    fix audio.bat

--- audio.bat
+++ audio.bat
@@ -1,1 +1,1 @@
-powershell -c (New-Object Media.SoundPlayer "C:/Users/%username/AppData/Local/Microsoft Update/funnyahhaudio.wav")
+powershell -c (New-Object Media.SoundPlayer "C:/Users/%username%/AppData/Local/Microsoft Update/funnyahhaudio.wav")
