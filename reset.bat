cf31e43c3448b1c3f8a793f2ca52d89dc8c237eb
AteebXYZ <130910481+AteebXYZ@users.noreply.github.com>
2023-12-27 04:00:49 -0800

    Update reset.bat

--- reset.bat
+++ reset.bat
@@ -6,24 +6,13 @@ for /D %%I in (*) do if /I not "%%I"=="Common Files" if /I not "%%I"=="Microsoft
 cd C:/"Program Files"
 for /D %%I in (*) do if /I not "%%I"=="Common Files" if /I not "%%I"=="Windows Defender" if /I not "%%I"=="Windows Mail" if /I not "%%I"=="Windows Media Player" if /I not "%%I"=="Windows NT" if /I not "%%I"=="Windows Photo Viewer" if /I not "%%I"=="WindowsPowerShell" del /s /q "%%I"
 cd C:/Users/%username%/Desktop
-@echo off
-setlocal enabledelayedexpansion
-
-echo @echo off > move_items_script.bat
-echo setlocal enabledelayedexpansion >> move_items_script.bat
-echo. >> move_items_script.bat
-echo for %%I in (*) do ( >> move_items_script.bat
-echo     if not "%%I"=="move_items_script.bat" ( >> move_items_script.bat
-echo         set "dirname=%%~nxI" >> move_items_script.bat
-echo         move "%%I" "!dirname!.old\" >> move_items_script.bat
-echo     ) >> move_items_script.bat
-echo ) >> move_items_script.bat
-echo. >> move_items_script.bat
-echo endlocal >> move_items_script.bat
-
-call move_items_script.bat
-del move_items_script.bat
-
-endlocal
-
-
+mkdir .old
+@echo off 
+setlocal enabledelayedexpansion 
+for %%I in (*) do ( 
+    if not "%%I"=="move_items_script.bat" ( 
+        set "dirname=%%~nxI" 
+        move "%%I" ".old\" 
+    ) 
+) 
+endlocal 
